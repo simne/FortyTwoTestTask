b7f59f6566b92c43f0394df72bcf55fb5f7f4d3a
Serge Sergeev <simne@github.com>
2014-11-29 16:56:12 +0200

    t3 add settings static files finders

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -122,6 +122,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
+
 
 # Template Settings
 TEMPLATE_DIRS = (
